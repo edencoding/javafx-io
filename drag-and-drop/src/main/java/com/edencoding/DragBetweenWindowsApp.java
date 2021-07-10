88ac341d8b6a380528acc98f495fe002fcfad3d9
Ed Eden-Rump <ed.edenrump@gmail.com>
2021-07-10 08:00:53 +0100

    rename files to be descriptive of purpose

--- drag-and-drop/src/main/java/com/edencoding/DragBetweenWindowsApp.java
+++ drag-and-drop/src/main/java/com/edencoding/DragBetweenWindowsApp.java
@@ -1,6 +1,6 @@
 package com.edencoding;
 
-import com.edencoding.controllers.DragAcrossController;
+import com.edencoding.controllers.DragNodeBetweenWindows;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
@@ -13,7 +13,7 @@ public class DragAcrossApp extends Application {
         //start primary window
         primaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/images/EdenCodingIcon.png")));
         primaryStage.setTitle("Drag and Drop example");
-        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/com/edencoding/fxml/dragShapeAcrossExample.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/com/edencoding/fxml/dragTextBetweenWindows.fxml"));
         Scene primaryScene = new Scene(fxmlLoader.load());
         primaryStage.setScene(primaryScene);
         primaryStage.setResizable(false);
@@ -24,9 +24,9 @@ public class DragAcrossApp extends Application {
         secondaryStage.getIcons().add(new Image(getClass().getResourceAsStream("/images/EdenCodingIcon.png")));
         secondaryStage.setTitle("Drag and Drop example");
         FXMLLoader loader = new FXMLLoader();
-        loader.setLocation(App.class.getResource("/com/edencoding/fxml/dragShapeAcrossExample.fxml"));
+        loader.setLocation(App.class.getResource("/com/edencoding/fxml/dragTextBetweenWindows.fxml"));
         Scene secondaryScene = new Scene(loader.load());
-        DragAcrossController controller = loader.getController();
+        DragNodeBetweenWindows controller = loader.getController();
         controller.deleteShape();
         secondaryStage.setScene(secondaryScene);
         secondaryStage.setResizable(false);
